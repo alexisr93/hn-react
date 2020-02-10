8b54e16baf72a6fcf4f6be921de9fde6aa432bf7
Alexis <alexisr1993@gmail.com>
2020-02-10 14:52:07 -0800

    Add functionality to NavBar

--- src/MainBody.js
+++ src/MainBody.js
@@ -5,12 +5,36 @@ class MainBody extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      current_nav: this.props.currentNav,
+      current_page: this.props.currentPage,
       stories: [],
     }
   }
 
-  componentDidMount() {
-    fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+  fetchData() {
+    var to_get = '';
+
+    switch(this.props.currentNav) {
+      case 'Top':
+        to_get = 'topstories';
+        break;
+      case 'New':
+        to_get = 'newstories';
+        break;
+      case 'Ask':
+        to_get = 'askstories';
+        break;
+      case 'Jobs':
+        to_get = 'jobstories';
+        break;
+      case 'Show':
+        to_get = 'showstories';
+        break;
+      default:
+        to_get = 'topstories';
+    }
+
+    fetch('https://hacker-news.firebaseio.com/v0/' + to_get + '.json')
       .then(res => res.json())
       .then(data => {
         this.setState({
@@ -18,9 +42,18 @@ class MainBody extends React.Component {
         });
       });
   }
+  
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate() {
+    this.fetchData();
+  }
+
   render() {
     var list_of_stories = (
-      this.state.stories.slice(0, 20).map(story =>
+      this.state.stories.slice((this.state.current_page * 1) - 1, this.state.current_page * 20).map(story =>
         <Story key={story} id={story}></Story>
       )
     )
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current_nav: this.props.current_nav
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.props.changeNav(event.target.id);
+  }
   render() {
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-dark">
@@ -19,19 +30,19 @@ class NavBar extends React.Component {
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Top</a>
+              <a className="nav-link" href="#" id="Top" onClick={this.handleClick}>Top</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">New</a>
+              <a className="nav-link" href="#" id="New" onClick={this.handleClick}>New</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Ask</a>
+              <a className="nav-link" href="#" id="Ask" onClick={this.handleClick}>Ask</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Jobs</a>
+              <a className="nav-link" href="#" id="Jobs" onClick={this.handleClick}>Jobs</a>
             </li>
             <li className="nav-item">
-              <a class="nav-link" href="#">Show</a>
+              <a className="nav-link" href="#" id="Show" onClick={this.handleClick}>Show</a>
             </li>
           </ul>
         </div>

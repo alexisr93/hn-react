3e9fc7582b0a40fd40807188a2bbc4d4bf8b81c9
Alexis <alexisr1993@gmail.com>
2020-02-10 12:42:12 -0800

    Minor changes

--- src/App.js
+++ src/App.js
@@ -10,25 +10,25 @@ function App() {
         <NavBar />
         <MainBody />
         <div className="container-fluid">
-          <nav aria-label="Page navigation example">
+          <nav aria-label="Page navigation temp">
             <ul className="pagination">
-              <li className="page-item"><a className="page-link" href="#">Previous</a></li>
-              <li className="page-item"><a className="page-link" href="#">1</a></li>
-              <li className="page-item"><a className="page-link" href="#">2</a></li>
-              <li className="page-item"><a className="page-link" href="#">3</a></li>
-              <li className="page-item"><a className="page-link" href="#">4</a></li>
-              <li className="page-item"><a className="page-link" href="#">5</a></li>
-              <li className="page-item"><a className="page-link" href="#">6</a></li>
-              <li className="page-item"><a className="page-link" href="#">7</a></li>
-              <li className="page-item"><a className="page-link" href="#">8</a></li>
-              <li className="page-item"><a className="page-link" href="#">9</a></li>
-              <li className="page-item"><a className="page-link" href="#">10</a></li>
-              <li className="page-item"><a className="page-link" href="#">11</a></li>
-              <li className="page-item"><a className="page-link" href="#">12</a></li>
-              <li className="page-item"><a className="page-link" href="#">13</a></li>
-              <li className="page-item"><a className="page-link" href="#">14</a></li>
-              <li className="page-item"><a className="page-link" href="#">15</a></li>
-              <li className="page-item"><a className="page-link" href="#">Next</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">Previous</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">1</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">2</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">3</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">4</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">5</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">6</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">7</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">8</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">9</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">10</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">11</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">12</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">13</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">14</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">15</a></li>
+              <li className="page-item"><a className="page-link text-dark" href="#">Next</a></li>
             </ul>
           </nav>
         </div>

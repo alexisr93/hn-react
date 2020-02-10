b099fe3846fd4a91037c2f0d309b6d0c33ea1272
Alexis <alexisr1993@gmail.com>
2020-02-09 21:16:25 -0800

    Add state variables

--- src/Story.js
+++ src/Story.js
@@ -4,22 +4,30 @@ class Story extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      headline: 'hello',
+      headline: 'When should I go to sleep',
+      domain: 'alexisramirez.net',
       link: '',
-      points: '',
-      by: '',
-      age: '',
-      num_of_comments: '',
+      points: '12',
+      by: 'alexis',
+      age: '2',
+      num_of_comments: '300',
       link_to_comments: '',
     }
   }
   render() {
     return (
-      <div>
-        <div className="row">
-          {this.state.headline}
-        </div>
-        <div className="row">
+      <div className="card my-2">
+        <div className="card-body">
+          <div className="card-subtitle">
+            {this.state.headline}
+            &nbsp;({this.state.domain})
+          </div>
+          <div className="card-text text-muted">
+            {this.state.points} points
+            by {this.state.by}
+            &nbsp;{this.state.age} hours ago |
+            &nbsp;{this.state.num_of_comments} comments
+          </div>
         </div>
       </div>
     );

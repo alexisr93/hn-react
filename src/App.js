9e4f2cb88c5a79165937e6197c4fff96a47dc020
Alexis <alexisr1993@gmail.com>
2020-02-10 14:53:26 -0800

    Fix handleChangeNav

--- src/App.js
+++ src/App.js
@@ -3,38 +3,109 @@ import './App.css';
 import NavBar from './NavBar.js';
 import MainBody from './MainBody';
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <NavBar />
-        <MainBody />
-        <div className="container-fluid">
-          <nav aria-label="Page navigation temp">
-            <ul className="pagination">
-              <li className="page-item"><a className="page-link text-dark" href="#">Previous</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">1</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">2</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">3</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">4</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">5</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">6</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">7</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">8</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">9</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">10</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">11</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">12</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">13</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">14</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">15</a></li>
-              <li className="page-item"><a className="page-link text-dark" href="#">Next</a></li>
-            </ul>
-          </nav>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current_nav: 'topstories',
+      current_page: 1
+    }
+    this.handleChangeNav = this.handleChangeNav.bind(this);
+  }
+
+  handleChangeNav(value) {
+    this.setState({
+      current_nav: value,
+      current_page: 1
+    });
+  }
+
+  handleClickPage(page) {
+    this.setState({
+      current_page: page
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <NavBar currentNav= {this.state.current_nav} changeNav={this.handleChangeNav} />
+          <MainBody currentNav={this.state.current_nav} currentPage={this.state.current_page} />
+          <div className="container-fluid">
+            <nav aria-label="Page navigation temp">
+              <ul className="pagination">
+                <li className="page-item">
+                  <a
+                    className="page-link text-dark"
+                    href="#"
+                  >
+                  Previous
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a
+                    className="page-link text-dark"
+                    href="#"
+                  >
+                  1
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a
+                    className="page-link text-dark"
+                    href="#"
+                  >
+                  2
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a
+                    className="page-link text-dark"
+                    href="#"
+                  >
+                  3
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a
+                    className="page-link text-dark"
+                    href="#"
+                  >
+                  6
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a
+                    className="page-link text-dark"
+                    href="#"
+                  >
+                  5
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a
+                    className="page-link text-dark"
+                    href="#"
+                  >
+                  6
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a
+                    className="page-link text-dark"
+                    href="#"
+                  >
+                  Next
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

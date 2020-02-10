108ad2842b697d5b1947a22b526a9cea3bb33edb
Alexis <alexisr1993@gmail.com>
2020-02-10 12:03:52 -0800

    Add fetch top stories

--- src/MainBody.js
+++ src/MainBody.js
@@ -2,29 +2,32 @@ import React from 'react';
 import Story from './Story.js';
 
 class MainBody extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stories: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          stories: data
+        });
+      });
+  }
   render() {
+    var list_of_stories = (
+      this.state.stories.slice(0, 20).map(story =>
+        <Story key={story} id={story}></Story>
+      )
+    )
+
     return (
       <div className="container-fluid">
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
-        <Story />
+        {list_of_stories}
       </div>
     );
   }

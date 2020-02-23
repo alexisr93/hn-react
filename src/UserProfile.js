b5beacfa3e51577efb268745265cc0d936885cb4
Alexis <alexisr1993@gmail.com>
2020-02-22 22:37:19 -0800

    Add fetch data

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -4,22 +4,39 @@ class UserProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: this.props.id,
+      id: window.location.pathname.slice(6),
       created: '',
       karma: '',
       about: '',
       submissions: [],
     }
   }
-  
+
+  componentDidMount() {
+    fetch('https://hacker-news.firebaseio.com/v0/user/' + this.state.id + '.json')
+      .then(res => res.json())
+      .then(data => {
+        let date = new Date(data.created * 1000).toLocaleDateString();
+
+        this.setState({
+          created: date,
+          karma: data.karma,
+          about: data.about,
+          submissions: data.submissions,
+        });
+      });
+  }
+
   render() {
     return (
-      <div>
-        user: {this.state.id}&nbsp;
-        created: {this.state.created}&nbsp;
-        karma: {this.state.karma}&nbsp;
-        about: {this.state.about}
-        <a href="#">submissions</a>
+      <div className="container-fluid">
+        <div className="container-fluid">
+          <div className="row">user: {this.state.id}</div>
+          <div className="row">created: {this.state.created}</div>
+          <div className="row">karma: {this.state.karma}</div>
+          <div className="row">about: {this.state.about}</div>
+          <div className="row"><a href="#">submissions</a></div>
+        </div>
       </div>
     );
   }
